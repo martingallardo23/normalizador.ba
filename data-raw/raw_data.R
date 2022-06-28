841cef23c23c786355b71e7e9af840972659f94b
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2022-06-28 00:00:40 -0300

    Add old usig

--- data-raw/raw_data.R
+++ data-raw/raw_data.R
@@ -1,4 +1,6 @@
 url_usig          <- "https://ws.usig.buenosaires.gob.ar/"
 url_usig_geocoder <- "https://ws.usig.buenosaires.gob.ar/geocoder/2.2/"
 url_usig_rest     <- "https://ws.usig.buenosaires.gob.ar/rest/"
-usethis::use_data(url_usig, url_usig_geocoder, url_usig_rest, overwrite = T)
+url_usig_old      <- "http://servicios.usig.buenosaires.gob.ar/normalizar/"
+usethis::use_data(url_usig, url_usig_geocoder, url_usig_rest,
+                  url_usig_old, overwrite = T)
